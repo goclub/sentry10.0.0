7f2039cb73ab00c0567a541cedc31274d12ce106
Nimo Chu <nimocc@126.com>
2025-10-21 11:44:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,11 +104,11 @@ fi
 echo ""
 echo "Fetching and updating Docker images..."
 echo ""
+echo "docker pull ${SENTRY_IMAGE:-getsentry/sentry:latest}"
 # We tag locally built images with an '-onpremise-local' suffix. docker-compose pull tries to pull these too and
 # shows a 404 error on the console which is confusing and unnecessary. To overcome this, we add the stderr>stdout
 # redirection below and pass it through grep, ignoring all lines having this '-onpremise-local' suffix.
 $dc pull -q --ignore-pull-failures 2>&1 | grep -v -- -onpremise-local || true
-echo "docker pull ${SENTRY_IMAGE:-getsentry/sentry:latest}"
 docker pull ${SENTRY_IMAGE:-getsentry/sentry:latest}
 
 echo ""
